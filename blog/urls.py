d717dd8d3bfabd8c1116c56c2729e8cab1cc6376
estebanriffo <estebanalejandro.riffo.lara@gmail.com>
2024-10-08 15:49:13 -0300

    parte jano jajaja

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name="inicio"
+app_name="blog"
 urlpatterns = [
-    path('', views.index,name="inicio"),
+    path('hola/', views.index,name="blog"),
 ]
